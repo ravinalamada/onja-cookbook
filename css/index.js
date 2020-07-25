ab399083746ae457af77dfcac42ab336ad8cb5db
Prisca Roseline <prisca.ros@onja.org>
2020-07-25 14:32:41 +0300

    I modified the input width

--- css/index.js
+++ css/index.js
@@ -8,6 +8,11 @@ const stepCont = document.querySelector('.step-container');
 const addStepBtn = document.querySelector('.add-step');
 const stepContent = document.querySelector('.cooking-step');
 const submitButton = document.querySelector('.submit-button');
+const recipeName = document.querySelector('#name');
+const image = document.querySelector('#image');
+const cookName = document.querySelector('#cook-name');
+const level = document.querySelector('#level');
+const duration = document.querySelector('#duration')
 
 // This function will creat Html in order to new add ingredients and append it into the DOM
 const handleNewIng = () => {
@@ -28,9 +33,35 @@ const handleSteps = () => {
   </div>
   `;
   stepCont.insertAdjacentHTML('beforeend', myHtml);
-}
+};
+
+const cookObject = () => {
+  return recipes = `[
+    {
+      title: '${recipeName.value}',
+      picture: '${image.value}',
+      author: '${cookName.value}',
+      difficulty: '${level.value}',
+      timing: '${duration.value}',
+      ingredients: ['eggs', 'salt', 'water'],
+      steps: [
+        'Put a pan on the fire',
+        'Crack the eggs on it',
+        'Wait, put them out',
+        'Add some salt on it',
+      ],
+    },
+  ]`;
+};
+
+
+const submitObj = () => {
+  const newObj = cookObject();
+  console.log(newObj);
+};
+
 // Listen to the all events
 
 addIngredientBtn.addEventListener('click', handleNewIng);
 addStepBtn.addEventListener('click', handleSteps);
-
+submitButton.addEventListener('submit', submitObj);
